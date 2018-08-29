543cecf42624df128e43c6145581378bbfecb051
gogonso <edycortesperez@gmail.com>
2018-08-29 07:28:46 -0500

    programa3

---
+++ Practica7/Ejercicio2/programa_31.cpp
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <iostream>
+#include <iostream>
+#include <time.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	string big_string;
+
+	srand (time(NULL));
+
+	for (int x = 0; x < atoi(argv[1]); x++)
+	{
+		string tls;
+		for (int z = 0; z <= 3; z++)
+		{
+			if(z==3){
+				tls.append(" ");
+			} else {
+				int i = rand() % 25 + 65;
+				char c = i;
+				string l(1,c);
+				tls.append(l);
+			}
+		}
+		big_string.append(tls);
+	}
+
+	int indice = big_string.find("IPN",0);
+
+	if(indice>0) {
+		cout << "'IPN' encontrado" << endl;
+	}
+}
