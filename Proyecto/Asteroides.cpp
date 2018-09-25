37031ef7e3e847bdfd0eaba3d03349ffa9e6c3bf
gogonso <edycortesperez@gmail.com>
2018-09-24 22:19:27 -0500

    Creación de asteroide

--- Proyecto/Asteroides.cpp
+++ Proyecto/Asteroides.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 class Poligono{
@@ -15,7 +16,24 @@ class Poligono{
         void crear_poligono(int, int,int);
         void trayectoria();
         void explosion();
+        void ast(int, int);
     };
+
+void Poligono::ast(int xf, int yf){
+    /*xf[conta]=radio*cos(0.01745329252*numa)+xo;
+    yf[conta]=radio*sin(0.01745329252*numa)+yo;
+    conta++;
+ while(numa<=180){    
+     radio = rand() % 20 + 18;
+     cout << radio << endl;
+    xf[conta]=radio*cos(0.01745329252*numa)+xo;
+    yf[conta]=radio*sin(0.01745329252*numa)+yo;
+     gfx_line(xf[conta-1],yf[conta-1],xf[conta],yf[conta]);
+     conta++;
+     numa=numa+20;
+     gfx_flush();
+ }*/
+}
     
 void Poligono::crear_poligono(int cX, int cY, int ld){
     coordX=cX;
@@ -38,7 +56,7 @@ void Poligono::trayectoria(){
         coordX=coordX+aux1;
         coordY=coordY+aux2;
          gfx_point(coordX, coordY);
-         cout << coordX << " , " << coordY <<endl;
+         //cout << coordX << " , " << coordY <<endl;
          gfx_flush();
          usleep(416);
     }else if(lado==1){
@@ -49,7 +67,7 @@ void Poligono::trayectoria(){
         coordX=coordX+aux1;
         coordY=coordY+aux2;
          gfx_point(coordX, coordY);
-         cout << coordX << " , " << coordY <<endl;
+         //cout << coordX << " , " << coordY <<endl;
          gfx_flush();
          usleep(416);
     }else if(lado==2){
@@ -83,15 +101,18 @@ void Poligono::explosion(){
 
 int main()
 {
- int num, coorX, coorY;
- Poligono a[200];
-    int cont=0,radio;
+    int num, coorX, coorY;
+    Poligono a[5];
+    int conta=0;
+    int cont=0, radio=30;
+    float numa=0;
+    float yf[60],xf[60],yo=300,xo=400;
     char c;
     srand(time(NULL));
     gfx_open(800, 600, "Proyecto 2 Equipo 4");
     gfx_color(0,200,100);
     coorY=rand()%601;
- while(cont<200){
+ while(cont<5){
      num = rand() % 4;//elegir de que lado saldra el poligono
      //Determinar de que lado va a salir y en que punto saldra el poligono
      if(num==0){
@@ -107,12 +128,26 @@ int main()
         coorX=rand()%801;
         coorY=600;
     }
-      a[cont].crear_poligono(coorX,coorY,num);
+      a[cont].crear_poligono(coorX,coorY,num);//24 por segundo
       cont++;
  }
  cont=0;
+    xf[conta]=radio*cos(0.01745329252*numa)+xo;
+    yf[conta]=radio*sin(0.01745329252*numa)+yo;
+    conta++;
+ while(numa<=360){    
+     radio = rand() % 20 + 18;
+     cout << radio << endl;
+    xf[conta]=radio*cos(0.01745329252*numa)+xo;
+    yf[conta]=radio*sin(0.01745329252*numa)+yo;
+     gfx_line(xf[conta-1],yf[conta-1],xf[conta],yf[conta]);
+     conta++;
+     numa=numa+20;
+     gfx_flush();
+ }gfx_line(xf[0],yf[0],xf[conta-1],yf[conta-1]);
+ 
  while(1){
-     if(cont==199)cont=0;
+     if(cont==5)cont=0;
      a[cont].trayectoria();
      cont++;
 }
