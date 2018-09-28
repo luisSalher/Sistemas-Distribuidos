8c25c1bac09e06d41766189936ac34335ae6251f
gogonso <edycortesperez@gmail.com>
2018-09-27 22:40:40 -0500

    Actualización, asteroides con movimiento

--- Proyecto/Asteroides.cpp
+++ Proyecto/Asteroides.cpp
@@ -10,88 +10,106 @@
 using namespace std;
 class Poligono{
     private:
-        int coordX;
-        int coordY;
-        int radio;
-        int lado;
+        int coordX;//coordenadas del centro del poligono
+        int coordY;//coordenadas del centro del poligono
+        int radio=30;//radio inicial del poligono
+        int lado;//lado del que va a salir el poligono
+        float yf[60];//arreglo de vertices en Y
+        float xf[60];//arreglo de vertices en X
+        int nvertices;//total de vertices del poligono
         
     public:
-        void crear_poligono(int, int,int);
         void trayectoria();
         void explosion();
-        void ast(int, int);
+        void ast(int, int, int);
 };
 
-void Poligono::ast(int xf, int yf){
-    /*xf[conta]=radio*cos(0.01745329252*numa)+xo;
-    yf[conta]=radio*sin(0.01745329252*numa)+yo;
-    conta++;
-
-    while(numa<=180){    
-        radio = rand() % 20 + 18;
-        cout << radio << endl;
-        xf[conta]=radio*cos(0.01745329252*numa)+xo;
-        yf[conta]=radio*sin(0.01745329252*numa)+yo;
-        gfx_line(xf[conta-1],yf[conta-1],xf[conta],yf[conta]);
-        conta++;
-        numa=numa+20;
-        gfx_flush();
-    }*/
-}
-    
-void Poligono::crear_poligono(int cX, int cY, int ld){
-    coordX=cX;
-    coordY=cY;
+void Poligono::ast(int xo, int yo, int ld){
+    int conta=0;
+    float numa=0;
     lado=ld;
-    radio = rand() % TAMANO_POLIGONO; //elegir el tamaño del poligono
-    gfx_point(coordX, coordY);
-	gfx_flush();
-    usleep(416);
-    return;
+    coordX=xo;
+    coordY=yo;
+//creacion del asteroide
+    xf[conta]=radio*cos(0.01745329252*numa)+coordX;
+    yf[conta]=radio*sin(0.01745329252*numa)+coordY;
+    conta++;
+ while(numa<=340){    
+     radio = rand() % 20 + 18;
+    xf[conta]=radio*cos(0.01745329252*numa)+coordX;
+    yf[conta]=radio*sin(0.01745329252*numa)+coordY;
+     gfx_line(xf[conta-1],yf[conta-1],xf[conta],yf[conta]);
+     conta++;
+     numa=numa+20;
+     gfx_flush();
+ }
+ gfx_line(xf[0],yf[0],xf[conta-1],yf[conta-1]); //termino de la creación del asteroide
+ nvertices=conta-1;
 }
 
 void Poligono::trayectoria(){
-    int aux1=0, aux2=0, num1=0,num2=0;
+    int aux1=0, aux2=0, num1=0,num2=0, h=0;
     if (lado==0){
         num1 = rand() % 2;
         num2 = rand() % 3-1;
-        aux1=aux1+num1;
-        aux2=aux2+num2;
-        coordX=coordX+aux1;
-        coordY=coordY+aux2;
-        gfx_point(coordX, coordY);
-        //cout << coordX << " , " << coordY <<endl;
-        gfx_flush();
+        xf[h]= xf[h] + num1;
+        yf[h]= yf[h] + num2;
+        for(h=1; h<=nvertices; h++){
+            xf[h]= xf[h] + num1;
+            yf[h]= yf[h] + num2;
+            coordX=coordX+num1;
+            coordY=coordY+num2;
+            gfx_line(xf[h-1],yf[h-1],xf[h],yf[h]);
+            gfx_flush();
+        }
+        gfx_line(xf[0],yf[0],xf[h-1],yf[h-1]);
         usleep(416);
     }else if(lado==1){
         num1 = rand() % 3-1;
         num2 = rand() % 2;
-        aux1=aux1+num1;
-        aux2=aux2+num2;
-        coordX=coordX+aux1;
-        coordY=coordY+aux2;
-        gfx_point(coordX, coordY);
-        //cout << coordX << " , " << coordY <<endl;
+        xf[h]= xf[h] + num1;
+        yf[h]= yf[h] + num2;
+        for(h=1; h<=nvertices; h++){
+            xf[h]= xf[h] + num1;
+            yf[h]= yf[h] + num2;
+            coordX=coordX+num1;
+            coordY=coordY+num2;
+            gfx_line(xf[h-1],yf[h-1],xf[h],yf[h]);
+            gfx_flush();
+        }
+        gfx_line(xf[0],yf[0],xf[h-1],yf[h-1]);
         gfx_flush();
         usleep(416);
     }else if(lado==2){
         num1 = rand() % 3-2;
         num2 = rand() % 3-1;
-        aux1=aux1+num1;
-        aux2=aux2+num2;
-        coordX=coordX+aux1;
-        coordY=coordY+aux2;
-        gfx_point(coordX, coordY);
+        xf[h]= xf[h] + num1;
+        yf[h]= yf[h] + num2;
+        for(h=1; h<=nvertices; h++){
+            xf[h]= xf[h] + num1;
+            yf[h]= yf[h] + num2;
+            coordX=coordX+num1;
+            coordY=coordY+num2;
+            gfx_line(xf[h-1],yf[h-1],xf[h],yf[h]);
+            gfx_flush();
+        }
+        gfx_line(xf[0],yf[0],xf[h-1],yf[h-1]);
         gfx_flush();
         usleep(416);
     }else if(lado==3){
         num1 = rand() % 3-1;
         num2 = rand() % 3-2;
-        aux1=aux1+num1;
-        aux2=aux2+num2;
-        coordX=coordX+aux1;
-        coordY=coordY+aux2;
-        gfx_point(coordX, coordY);
+        xf[h]= xf[h] + num1;
+        yf[h]= yf[h] + num2;
+        for(h=1; h<=nvertices; h++){
+            xf[h]= xf[h] + num1;
+            yf[h]= yf[h] + num2;
+            coordX=coordX+num1;
+            coordY=coordY+num2;
+            gfx_line(xf[h-1],yf[h-1],xf[h],yf[h]);
+            gfx_flush();
+        }
+        gfx_line(xf[0],yf[0],xf[h-1],yf[h-1]);
         gfx_flush();
         usleep(416);
     }
@@ -106,10 +124,7 @@ void Poligono::explosion(){
 int main(){
     int num, coorX, coorY;
     Poligono a[NUMERO_ASTEROIDES];
-    int conta=0;
-    int cont=0, radio=30;
-    float numa=0;
-    float yf[60],xf[60],yo=300,xo=400;
+    int cont=0;
     char c;
     srand(time(NULL));
     gfx_open(800, 600, "Proyecto 2 Equipo 4");
@@ -132,37 +147,16 @@ int main(){
             coorX=rand()%801;
             coorY=600;
         }
-        
-        a[cont].crear_poligono(coorX,coorY,num);//24 por segundo
+        a[cont].ast(coorX,coorY,num);
         cont++;
     }
-
-    cont=0;
-    xf[conta]=radio*cos(0.01745329252*numa)+xo;
-    yf[conta]=radio*sin(0.01745329252*numa)+yo;
-    conta++;
-
-    while(numa<=360){ 
-
-        radio = rand() % 20 + 18;
-        cout << radio << endl;
-        xf[conta]=radio*cos(0.01745329252*numa)+xo;
-        yf[conta]=radio*sin(0.01745329252*numa)+yo;
-        gfx_line(xf[conta-1],yf[conta-1],xf[conta],yf[conta]);
-        conta++;
-        numa=numa+20;
-        gfx_flush();
-
-    }
-
-    gfx_line(xf[0],yf[0],xf[conta-1],yf[conta-1]);
 
     while(1){
-        if(cont==NUMERO_ASTEROIDES){
-            cont=0;
-        }
+        
+        for(cont=0;cont<NUMERO_ASTEROIDES;cont++)
         a[cont].trayectoria();
-        cont++;
+        
+        gfx_clear();
     }
     return 0;
 }
