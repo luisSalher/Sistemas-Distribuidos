12858ff293b3f75bab3b76ec58eadc8ec0c6e108
edyCortesP <edycortesperez@gmail.com>
2018-10-07 16:50:01 -0500

    Actualizacion Servidor.cpp p12

--- Practica12/Servidor.cpp
+++ Practica12/Servidor.cpp
@@ -1,8 +1,11 @@
 #include "SocketDatagrama.h"
+#include <iostream>
 #define PUERTO 7200
 
 int main(int argc, char const *argv[]) {
     printf("%s\n", "SERVIDOR");
+    unsigned int *num;
+    int primo;
 
     SocketDatagrama socketCliente(PUERTO);
     PaqueteDatagrama paquete(2*sizeof(double));
@@ -10,7 +13,23 @@ int main(int argc, char const *argv[]) {
     while(1) {
         printf("%s\n", "ESPERANDO...");
         printf("Recibidos = %d\n", socketCliente.recibe(paquete));
-        printf("Mensaje de: %s:%d\n", paquete.obtieneDireccion(), paquete.obtienePuerto());   
+        num=(unsigned  int *)paquete.obtieneDatos();
+        cout << "Numero a analizar: " << num[0] << endl;
+        cout << "Limite inferior: " << num[1] << endl;
+        cout << "Limite superior: " << num[2] << endl;
+        
+        printf("Mensaje de: %s:%d\n", paquete.obtieneDireccion(), paquete.obtienePuerto());
+        
+        primo = 0;
+        for(int i = num[1]; i < num[2]; i++){
+            if(num[0] % i ==0){
+                primo = 1;
+                i = num[2];
+            }
+        }
+        cout << primo << endl;
+        PaqueteDatagrama p((char *)&primo, sizeof(primo) * sizeof(int), paquete.obtieneDireccion(), paquete.obtienePuerto());
+        socketCliente.envia(p);
     }
 
     return 0;
