cda0b4d29a9ad1756ac190745408d052d5dc023c
luisSalher <luis.salinas1086@gmail.com>
2018-10-15 20:45:47 -0500

    Se corrigió un error en el contructor de la clase PaqueteDatagrama

--- Practica13/Servidor.cpp
+++ Practica13/Servidor.cpp
@@ -1,33 +1,26 @@
 #include "SocketDatagrama.h"
 #include "Mensaje.h"
 #include "Respuesta.h"
-#include <cstdio>
-#include <vector>
-#include <cstring>
-#include <iostream>
-#include <string>
 using namespace std;
 
 int main(int argc, char*argv[]) {
+    int longitudCadena = 0;
+    int contador = 0;
+    int i = 0;
+    int j = 0;
     Respuesta respuesta(7200);
 
     while(true) {
         printf("%s\n", "ESPERANDO...");
         struct mensaje *msg = respuesta.getRequest();
 
-        string cadenarecibida = msg->arguments;
+        string cadenaRecibida = msg->arguments;
         string cadenaInvertida = "";
-        printf("%s\n", cadenaInvertida.c_str());
+        longitudCadena = cadenaRecibida.length();
 
         // Se invierten las palabras de la cadena
-        for(i = ; i<strlen(cadenarecibida); i++){
-            if(cadenarecibida[i] != ' '){
-                cadenaInvertida[i] = cadenarecibida[i];
-            }
-        }
-
-
-
+        
         respuesta.sendReply((char *)cadenaInvertida.c_str(), msg->IP, msg->puerto);
     }
+
 }
