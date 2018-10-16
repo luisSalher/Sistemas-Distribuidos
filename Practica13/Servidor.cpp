1bb46c13371885ca65d311e0fee45c3a11c8a72b
edyCortesP <edycortesperez@gmail.com>
2018-10-16 01:01:31 -0500

    codigo invertir, ya invierte las palabras

--- Practica13/Servidor.cpp
+++ Practica13/Servidor.cpp
@@ -1,7 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
 #include "SocketDatagrama.h"
 #include "Mensaje.h"
 #include "Respuesta.h"
+
+#define TAM 200
+char cadena2[TAM];
 using namespace std;
+
+void invertir(char cadena[]){
+    int cont=0;
+    memset(cadena2,'\0',TAM);
+    int tam=strlen(cadena);
+    int aux;
+    for(tam; tam>=0; tam--){
+        if(cadena[tam]==' '){
+            aux = tam+1;
+            while(cadena[aux] !=' '  ){
+                if(aux==strlen(cadena))
+                    break;
+                cadena2[cont++]=cadena[aux];
+                aux++;
+            }
+            cadena2[cont++]=' ';
+        }
+        if(tam==0){ 
+            aux = tam;
+            while(cadena[aux] !=' '){  
+                cadena2[cont++]=cadena[aux];
+                aux++;
+            }
+
+        }
+    }
+}
 
 int main(int argc, char*argv[]) {
     int longitudCadena = 0;
@@ -15,12 +48,14 @@ int main(int argc, char*argv[]) {
         struct mensaje *msg = respuesta.getRequest();
 
         string cadenaRecibida = msg->arguments;
-        string cadenaInvertida = "";
-        longitudCadena = cadenaRecibida.length();
-
-        // Se invierten las palabras de la cadena
         
+        longitudCadena = cadenaRecibida.length();
+        char* cadena = new char[longitudCadena + 1];
+        strcpy(cadena,cadenaRecibida.c_str());
+        invertir(cadena);
+        string cadenaInvertida(cadena2);
         respuesta.sendReply((char *)cadenaInvertida.c_str(), msg->IP, msg->puerto);
     }
 
 }
+
